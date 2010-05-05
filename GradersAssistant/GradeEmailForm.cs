b9eb93e9dc0bdb329e4a711501a8730fa56d2fe8
jpereyda11 <jpereyda11@LAB-LIN-201-08.whitworth.edu>
2010-05-05 15:51:11 -0700

    Messed with the grade emailing stuff, works with gmail now!  Thanks Josh.

--- GradersAssistant/GradeEmailForm.cs
+++ GradersAssistant/GradeEmailForm.cs
@@ -13,7 +13,7 @@ namespace GradersAssistant
 {
     public partial class GradeEmailForm : Form
     {
-        public GradeEmailForm()
+        public GradeEmailForm()//Dictionary<int, Student> students
         {
             InitializeComponent();
         }
@@ -27,8 +27,54 @@ namespace GradersAssistant
         {
             if (radioButtonProtocolExchange.Checked)
             {
-                EmailTest(textBoxEmailAddress.Text, textBoxExchangePassword.Text);
+                //EmailTest(textBoxEmailAddress.Text, textBoxExchangePassword.Text);
             }
+            sendEmail(radioButtonProtocolExchange.Checked, "smtp.gmail.com", textBoxEmailAddress.Text, textBoxExchangePassword.Text, "This is a spoof email!", "Hi cousin!", "raptorcantor@gmail.com");
+        }
+
+        private bool sendEmail(bool authenticated, string host, string username, string password, string text, string subject, string recipient)
+        {
+            if (!username.Contains('@'))
+            {
+                MessageBox.Show("Please enter a valid email address.", "Invalid Email!");
+                return false;
+            }
+            if (!recipient.Contains('@'))
+            {
+                MessageBox.Show("The recipient \"" + recipient + "\" has an invalid email address.");
+                return false;
+            }
+            SmtpClient smtpClient = new SmtpClient();
+            NetworkCredential theCredential = new NetworkCredential(username, password);
+            MailMessage message = new MailMessage();
+            MailAddress fromAddress = new MailAddress("josh.simmons@gmail.com");
+
+            smtpClient.Host = host;
+            if (authenticated)
+            {
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Port = 587;
+                smtpClient.EnableSsl = true;
+                //smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            }
+            smtpClient.Credentials = theCredential;
+
+            message.From = fromAddress;
+            message.Subject = subject;
+            message.IsBodyHtml = false;
+            message.Body = text;
+            message.To.Add(recipient);
+            
+            try
+            {
+                smtpClient.Send(message);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error sending email:\n" + ex.Message, ex.Message);
+            }
+            return false;
         }
 
         public void EmailTest(string username, string password)
@@ -40,14 +86,17 @@ namespace GradersAssistant
 
             //smtpClient.Host = "bl2prd0102.outlook.com";// bl2prd0102.outlook.com is the student outlook server, maybe this should be an option?
             smtpClient.Host = "hub1.whitworth.edu";
-            smtpClient.UseDefaultCredentials = false;
             smtpClient.Credentials = theCredential;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Port = 587;
+            
 
             message.From = fromAddress;
             message.Subject = "Hey Cousin!";
             message.IsBodyHtml = false;
             message.Body = "Hey Josh,\nIt turns out we're second cousins, and all my money got stolen by some Nigerian scam artists.\n\nIf you could send me ten thousand dollars, I will be able to open my bank account and get my money back.  I'd be pleased to give you a half percent of my estate in exchange for the assistance.\n\nYour cousin,\nBill";
-            message.To.Add("jsimmons10@my.whitworth.edu");
+            message.To.Add("raptorcantor@gmail.com");
+            //message.To.Add("@my.whitworth.edu");
 
             try
             {
